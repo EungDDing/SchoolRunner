3cdfc76a359b23d1c6ae0f0af2c9d0659a203145
이응민 <ppoppo9802@naver.com>
2025-03-25 16:57:31 +0900

    Update 0235

--- Assets/01Script/Obstacle.cs
+++ Assets/01Script/Obstacle.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour, IScroll
 {
     [SerializeField] private float scrollSpeed;
+    [SerializeField] private float flyForce;
     private PlayerController playerController;
+    private Vector3 flyDir = new Vector3(-1.0f, 1.0f, 0.0f);
+    private Rigidbody rig;
+
     private int damage = 1;
     private void Awake()
     {
         GameObject obj = GameObject.FindGameObjectWithTag("Player");
         obj.TryGetComponent<PlayerController>(out playerController);
+
+        gameObject.TryGetComponent<Rigidbody>(out rig);
         scrollSpeed = 10.0f;
+        flyForce = 7.0f;
     }
+    
     private void Update()
     {
         Scroll();
@@ -27,6 +35,7 @@ public class Obstacle : MonoBehaviour, IScroll
         if (other.gameObject.CompareTag("Player"))
         {
             playerController.TakeDamage(damage);
+            rig.AddForce(flyDir * flyForce, ForceMode.Impulse);
         }
     }
 }
