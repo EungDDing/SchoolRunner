db74ced1a71232320a280bd74f7886aedd6060b8
xii-iix <xiixsh12@gmail.com>
2025-04-25 03:14:53 +0900

    update 250425 03:14
    1스테이지 오른쪽벽 프리팹 완료
    (창문 애니메이션, 스크립트, 피벗)
    1스테이지 바닥 추가
    (텍스처파일, 노말맵 추가)

---
+++ Assets/03Art/Background/Source/1Stage/1Stage right wall/WindowTrigger.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class WindowTrigger : MonoBehaviour
+{
+    public Animator window1;
+    public Animator window2;
+
+    private bool triggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (triggered) return; // 중복 방지
+        if (!other.CompareTag("Player")) return;
+
+        triggered = true;
+
+        // 창문마다 랜덤으로 열릴지 결정
+        if (Random.value > 0.5f)
+        {
+            window1.SetTrigger("Open");
+        }
+        if (Random.value > 0.5f)
+        {
+            window2.SetTrigger("Open");
+        }
+
+        Debug.Log("창문 랜덤 열림 처리 완료!");
+    }
+}
