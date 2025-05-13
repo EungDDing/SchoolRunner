7ef5c1c166e566a1bd29442b50ecef9aeae1f2ed
이응민 <ppoppo9802@naver.com>
2025-05-13 16:25:38 +0900

    Update 0513

--- Assets/02Scenes/RunningScene_Profiles/PostProcessManager.cs
+++ Assets/02Scenes/RunningScene_Profiles/PostProcessManager.cs
@@ -3,18 +3,49 @@ using UnityEngine.Rendering.PostProcessing;
 
 public class PostProcessChanger : MonoBehaviour
 {
-    public PostProcessVolume postProcessVolume;
-    public PostProcessProfile[] stageProfiles;
+    [SerializeField] private PostProcessVolume postProcessVolume;
+    [SerializeField] private PostProcessProfile[] stageProfiles;
 
-    public void ChangePostProcessProfile(int stageIndex)
+    private int stageCount;
+    private void Awake()
     {
-        if (stageIndex >= 0 && stageIndex < stageProfiles.Length)
+        Stage.OnChangeStageCount += StageCount;
+    }
+    public void ChangePostProcessProfile()
+    {
+        if (stageCount % 5 == 0)
         {
-            postProcessVolume.profile = stageProfiles[stageIndex];
+            Debug.Log("post process Bridge");
+            postProcessVolume.profile = stageProfiles[4];
+        }
+        else if (stageCount / 5 == 0)
+        {
+            Debug.Log("post process 01");
+            postProcessVolume.profile = stageProfiles[0];
+        }
+        else if (stageCount / 5 == 1)
+        {
+            Debug.Log("post process 02");
+            postProcessVolume.profile = stageProfiles[1];
+        }
+        else if (stageCount / 5 == 2)
+        {
+            Debug.Log("post process 03");
+            postProcessVolume.profile = stageProfiles[2];
+        }
+        else if (stageCount / 5 == 3)
+        {
+            Debug.Log("post process 04");
+            postProcessVolume.profile = stageProfiles[3];
         }
         else
         {
             Debug.LogWarning("스테이지 인덱스 잘못 들어감!");
         }
+    }
+    private void StageCount()
+    {
+        stageCount++;
+        ChangePostProcessProfile();
     }
 }
