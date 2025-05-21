881ce4875b4fbd58498fbd158d06530486084eac
이응민 <ppoppo9802@naver.com>
2025-05-22 06:35:50 +0900

    Update 0522

---
+++ Assets/01Script/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    [SerializeField] private AudioSource bgmPlayer;
+    [SerializeField] private List<AudioClip> bgmList;
+
+    [SerializeField] private List<AudioSource> sfxPlayer;
+    [SerializeField] private List<AudioClip> sfxList;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
